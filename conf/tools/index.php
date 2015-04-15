42d855fedb229afc5409f2c91ee7aec151d32ce9
Ron Williams <hello@ronwilliams.io>
2015-04-15 10:13:15 -0600

    Change menu order and change breakpoint for mobile devices.

--- conf/tools/index.php
+++ conf/tools/index.php
@@ -16,12 +16,15 @@
 
     <!-- Optional theme -->
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+    
+    <!-- Custom styles for this template -->
+    <link href="index/justified-nav.css" rel="stylesheet">
+
+    <!-- jQuery CDN -->
+    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
 
     <!-- Latest compiled and minified JavaScript -->
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-
-    <!-- Custom styles for this template -->
-    <link href="index/justified-nav.css" rel="stylesheet">
 
   </head>
 
@@ -38,8 +41,8 @@
             <li class="active"><a href="#">Dashboard</a></li>
             <li><a href="adminer.php">Adminer</a></li>
             <li><a href="<?php print $mailcatcher; ?>">Mailcatcher</a></li>
-            <li><a href="phpinfo.php">phpinfo</a></li>
             <li><a href="logs/">Logs</a></li>
+            <li><a href="phpinfo.php">phpinfo</a></li>
             <li><a href="http://usda-ocio-eas.github.io/lap-docker/news/">News</a></li>
             <li><a href="http://usda-ocio-eas.github.io/lap-docker/contrib/">Contrib</a></li>
             <li><a href="http://usda-ocio-eas.github.io/lap-docker/docs/">Docs</a></li>
@@ -56,18 +59,18 @@
 
       <!-- Example row of columns -->
       <div class="row">
-        <div class="col-lg-4">
+        <div class="col-md-4">
           <h2>Dashboard Usage!</h2>
           <p class="text-danger">The dashboard links are set to use default ports.</p>
           <p>Mailcatcher, Adminer, and other dashboard links expect the default quickstart port mapping configurations. Using non-standard ports may direct to other Virtual Machines or not respond. </p>
           <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
         </div>
-        <div class="col-lg-4">
+        <div class="col-md-4">
           <h2>Support</h2>
           <p>If you encounter a bug with the LAP Docker software, we want to hear from you! Send us a note and we'll be sure to fix or help you resolve the issue. </p>
           <p><a class="btn btn-primary" href="mailto:rone.williams@ocio.usda.gov" role="button">Contact &raquo;</a></p>
        </div>
-        <div class="col-lg-4">
+        <div class="col-md-4">
           <h2>Contribute</h2>
           <p>LAP Docker is an open source software dedicated to providing rapid web application development, testing, and debugging. If you'd like to contribute to LAP Docker, get it touch.</p>
           <p><a class="btn btn-primary" href="mailto:rone.williams@ocio.usda.gov" role="button">Contribute &raquo;</a></p>
@@ -80,9 +83,5 @@
       </footer>
 
     </div> <!-- /container -->
-
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
 </html>
